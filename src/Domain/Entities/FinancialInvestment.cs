7bf9674f9b18a35bed20af5ee359866d00c8ba99
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-02-20 16:40:15 -0300

    feat: financialInvestment entity and related enums

---
+++ src/Domain/Entities/FinancialInvestment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class FinancialInvestment
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int NumberOfShares { get; set; }
+    public double ProfitPerShare { get; set; }
+    public double TotalProfit { get; set; }
+    public double PricePerShare { get; set; }
+    public EFinancialInvestmentType Type { get; set; }
+    public int PurchaseDay { get; set; }
+    public int PayDay { get; set; }
+    public EFrequency Frequency { get; set; }
+    public double TotalSpent { get; set; }
+    public string Url { get; set; }
+}
---
+++ src/Domain/Enums/EFinancialInvestmentType.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Enums;
+
+public enum EFinancialInvestmentType
+{
+    CorporateFloors,
+    Logistics,
+    Shopping,
+    UrbanIncome,
+    Paper,
+    Agriculture,
+    Hybrid,
+    FundOfFunds,
+    Others,
+    Stocks
+}
