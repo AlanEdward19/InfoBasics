57e229f3ec18b5ad8f637bba67bb4993f704cb96
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-02-20 16:38:11 -0300

    feat: migrations

---
+++ src/Infrastructure/Migrations/20240220180138_InvestmentTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InvestmentTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FinancialInvestments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumberOfShares = table.Column<int>(type: "int", nullable: false),
+                    ProfitPerShare = table.Column<double>(type: "float", nullable: false),
+                    TotalProfit = table.Column<double>(type: "float", nullable: false),
+                    PricePerShare = table.Column<double>(type: "float", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    PurchaseDay = table.Column<int>(type: "int", nullable: false),
+                    PayDay = table.Column<int>(type: "int", nullable: false),
+                    Frequency = table.Column<int>(type: "int", nullable: false),
+                    TotalSpent = table.Column<double>(type: "float", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FinancialInvestments", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FinancialInvestments");
+        }
+    }
+}
